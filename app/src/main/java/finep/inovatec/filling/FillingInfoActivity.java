4db095fde4443544660ec767832fc3c22530a550
Nivaldo Bondanca <hick209@gmail.com>
2015-10-13 22:16:06 -0300

    Created filling info layout resource

--- app/src/main/java/finep/inovatec/filling/FillingInfoActivity.java
+++ app/src/main/java/finep/inovatec/filling/FillingInfoActivity.java
@@ -2,9 +2,13 @@ package finep.inovatec.filling;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
+import finep.inovatec.R;
 import finep.inovatec.app.BaseActivity;
+import finep.inovatec.data.Filling;
+import finep.inovatec.databinding.ActivityFillingInfoBinding;
 
 /**
  * @author Nivaldo Bondança
@@ -17,8 +21,16 @@ public class FillingInfoActivity extends BaseActivity {
 
 	// TODO
 
+	private FillingDetailsViewModel mViewModel;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		Filling filling = null; // TODO
+		mViewModel = new FillingDetailsViewModel(this, filling);
+
+		ActivityFillingInfoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_filling_info);
+		binding.setViewModel(mViewModel);
 	}
 }
